1254f74c0077e72dab530af7cc53a04ef331bd3f
wsmn <wsmn123123@163.com>
2019-08-19 10:08:39 +0800

    fixed connect error

--- src/Util/DBConnect.java
+++ src/Util/DBConnect.java
@@ -6,9 +6,9 @@ import java.util.Properties;
 import java.util.concurrent.Executor;
 
 public class DBConnect {
-    private static final String driver = "com.mysql.jdbc.Driver"; //数据库驱动
+    private static final String driver = "com.mysql.cj.jdbc.Driver"; //数据库驱动
     //连接数据库的URL地址
-    private static final String url = "jdbc:mysql://localhost:3306/library?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+    private static final String url = "jdbc:mysql://localhost:3306/library?useUnicode=true&characterEncoding=UTF-8&useSSL=false"+"&serverTimezone=GMT%2B8";
     private static final String username = "root";//数据库的用户名
     private static final String password = "123456";//数据库的密码
 
